1e1d3d685c443dda24df4b15a880e62d22fab5be
Genesis Gonzalez <18-11019@usb.ve>
2021-04-28 16:11:25 -0400

    Añadimos el carousel. Pendiente ajustes en la version movil

--- js/app.js
+++ js/app.js
@@ -10,4 +10,13 @@ addEventListener('DOMContentLoaded', () => {
 });
 
 //carousel
-document.addEventListener
+document.addEventListener('DOMContentLoaded', () => { 
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration:300,
+        dist:-80,
+        shift:5,
+        padding: 5,
+        indicators: true
+    });
+});
